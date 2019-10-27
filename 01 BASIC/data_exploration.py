75bb963705d0c5c2457ff2d3a7c359f4a0252f48
WeddingCandy <chenyouwenxyz@hotmail.com>
2019-10-27 21:40:44 +0800

    10/27/2019  commit on mac

--- 01 BASIC/data_exploration.py
+++ 01 BASIC/data_exploration.py
@@ -10,9 +10,19 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-df_train = pd.read_csv('../01 BASIC/data/train.csv')
 
+df_train = pd.read_csv('../01 BASIC/data/train.csv')
 
 print(df_train.columns)
 
 
+#descriptive statistics summary
+print(df_train['SalePrice'].describe())
+
+#histogram
+sns.distplot(df_train['SalePrice'])
+# plt.show()
+
+#skewness and kurtosis
+print("Skewness: %f" % df_train['SalePrice'].skew())
+print("Kurtosis: %f" % df_train['SalePrice'].kurt())
