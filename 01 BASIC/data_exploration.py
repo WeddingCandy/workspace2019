566f9c255c9af4766ccd3bbf57b4b36bf67a88d5
Chans <chenyouwenxyz@hotmail.com>
2019-10-29 19:30:55 +0800

    10/29/2019 commit on PC firt

--- 01 BASIC/data_exploration.py
+++ 01 BASIC/data_exploration.py
@@ -67,6 +67,14 @@ cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
 cm = np.corrcoef(df_train[cols].values.T)
 sns.set(font_scale=1.25)
 hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+
+
+#scatterplot
+sns.set()
+cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+sns.pairplot(df_train[cols], size = 2.5)
+
+
 plt.show()
 
 
